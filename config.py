b775d08b083518235f918639959717cef2c9ec65
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-03 08:44:14 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- config.py
+++ config.py
@@ -69,7 +69,7 @@ STRING5 = getenv("STRING_SESSION5")
 
 # ── Media assets ───────────────────────────────────────────────────────────────
 START_VIDS = [
-    "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
+    "https://files.catbox.moe/h25hgt.mp4",
     "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
     "https://telegra.ph/file/a4d90b0cb759b67d68644.mp4",
 ]
