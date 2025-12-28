1ce2d007bc9d9ac3d02fa17af3b307d1b387de34
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-28 11:56:36 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- config.py
+++ config.py
@@ -31,9 +31,9 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "30"))
 
 # ── External APIs ──────────────────────────────────────────────────────────────
 COOKIE_URL = getenv("COOKIE_URL")  # required (paste link)
-API_URL = getenv("API_URL")        # optional
-VIDEO_API_URL = getenv("VIDEO_API_URL")  # optional
-API_KEY = getenv("API_KEY")        # optional
+#API_URL = https://teaminflex.xyz        # optional
+#VIDEO_API_URL = getenv("VIDEO_API_URL")  # optional
+#API_KEY = getenv("API_KEY")        # optional
 DEEP_API = getenv("DEEP_API")      # optional
 
 # ── Hosting / deployment ───────────────────────────────────────────────────────
