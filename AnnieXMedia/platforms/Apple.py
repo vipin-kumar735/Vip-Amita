a0d674406b2deacc5196f7b4ef49d92f970790b4
vipin-kumar735 <patelharimohan31@gmail.com>
2026-01-01 08:40:20 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -3,7 +3,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.__future__ import VideosSearch
+#from youtubesearchpython.__future__ import VideosSearch
 
 
 class AppleAPI:
