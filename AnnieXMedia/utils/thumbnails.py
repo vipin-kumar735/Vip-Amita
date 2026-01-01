899c46bf0838c20acbaf2f7b1e4cf5b65d77fcb5
vipin-kumar735 <patelharimohan31@gmail.com>
2026-01-01 08:57:33 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- AnnieXMedia/utils/thumbnails.py
+++ AnnieXMedia/utils/thumbnails.py
@@ -3,7 +3,7 @@ import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 from config import YOUTUBE_IMG_URL
 from AnnieXMedia.core.dir import CACHE_DIR 
 
