044e7de204956bff76e8680d738344296fc28559
vipin-kumar735 <patelharimohan31@gmail.com>
2026-01-01 09:11:16 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- AnnieXMedia/platforms/Spotify.py
+++ AnnieXMedia/platforms/Spotify.py
@@ -2,7 +2,8 @@ import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
+
 
 import config
 
