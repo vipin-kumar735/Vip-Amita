a2f5be1b3cf72d06da89f97910782821e04012a1
vipin-kumar735 <patelharimohan31@gmail.com>
2026-01-01 09:05:44 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- AnnieXMedia/plugins/bot/start.py
+++ AnnieXMedia/plugins/bot/start.py
@@ -4,7 +4,7 @@ import time
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 import config
 from AnnieXMedia import app
